4396324a668732bebff71234f1a4b57e5ae03a25
Ljbosman1 <ljbosman2@gmail.com>
2022-11-11 22:24:52 +0200

    Add comment

--- backend/employees/tests/test_services.py
+++ backend/employees/tests/test_services.py
@@ -1,2 +1,3 @@
 # No Need to test generate_employee_id because that would be just testing python's random & string libraries.
 # I'm sure they did a proper job themselves testing that
+# Just have this file here for showing how I lay out my tests
