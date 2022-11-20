44bb6e84c5dccef8c4897923714cd6067164d8d9
Ljbosman1 <ljbosman2@gmail.com>
2022-11-20 21:08:07 +0200

    Add fix for mapping skills bug

--- frontend/src/redux/selectors.js
+++ frontend/src/redux/selectors.js
@@ -1,5 +1,17 @@
+import { mapSnakeToCamel } from "../utils/utils"
+
 export const getSkillsByEmployeeId = (store, employeeId) => {
-  const allSkills = store.skills;
+  const allSkills = [...store.skills];
+  // This is a hacky fix for a weird mapping bug where stateSkills and 
+  // skills objects get saved in snake case to the store after editing
+  // an employee
+  allSkills.every(skill => {
+    if (Object.keys(skill).filter(key => key.includes("_")).length > 0) {
+      mapSnakeToCamel(skill);
+      return false;
+    }
+    return true
+  })
   return employeeId ? (
     allSkills.filter(
       skill => skill.employeeId === employeeId
